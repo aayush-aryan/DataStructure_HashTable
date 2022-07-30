fd7f77e7ea9af27dc4f7e9cbab9c2fc465049563
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-30 21:20:44 +0530

    added. finding frequency of words in a sentences

--- Section3HashTable/Program.cs
+++ Section3HashTable/Program.cs
@@ -7,7 +7,9 @@ namespace Section3HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To HashTables Section 3 !");
-
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To be or not to be");
+            hash.frequencyOfWords("0");
         }
     }
 }
